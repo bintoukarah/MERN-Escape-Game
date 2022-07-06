2c6e4cc4d74d4d42f205794e1db2581de00153e6
bkaramo <bintoukaramoko3@gmail.com>
2022-07-06 17:51:41 +0200

    add cors dependencies

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 const app = express();
 import Mongoose from 'mongoose';
+import cors from 'cors'
 
 // import routes
 import userRoutes from  './routes/userRoutes.js';
@@ -9,7 +10,7 @@ import bookingRoutes from  './routes/bookingRoutes.js';
 
 const PORT = 5000;
 
-
+app.use(cors())
 app.use('/users', userRoutes )
 app.use('/rooms', roomRoutes )
 app.use('/bookings', bookingRoutes )
